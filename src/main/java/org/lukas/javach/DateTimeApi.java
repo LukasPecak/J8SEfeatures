44fc7e867260fcf6747d4cf8ea970b8a8ab88be1
LukasPecak <lukas.pecak@gmail.com>
2017-07-04 22:37:08 +0200

    Date plusYears leap year cases

--- src/main/java/org/lukas/javach/DateTimeApi.java
+++ src/main/java/org/lukas/javach/DateTimeApi.java
@@ -8,7 +8,17 @@ import java.time.LocalDate;
  * @author Lukas Pecak
  */
 class DateTimeApi {
+    /**
+     *  Compute Programmers's Day without using plusDays method
+     */
     LocalDate computeProgrammersDay(int year) {
         return LocalDate.ofYearDay(year, 256);
+    }
+
+    /**
+     *  What happens when you add one year to LocalDate.of(2000, 2, 29)? Four years ?
+     */
+    LocalDate addYears(long years) {
+        return LocalDate.of(2000, 2, 29).plusYears(years);
     }
 }
